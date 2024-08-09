24bf193e62d7618469a484ffc79ed1687affc0e4
arush <arush.up@gmail.com>
2024-08-10 01:18:02 +0530

    initial commit

---
+++ Components/Models/SquareModel.cs
@@ -0,0 +1,22 @@
+namespace BlazorGame.Components.Models
+{
+    public class SquareModel
+    {
+        public bool isShown;
+
+        public int Id {get;set;}
+        public static string? Style{get;set;}
+        public bool IsShown{
+            get => isShown;
+            set {
+                isShown = value;
+                if ( isShown){
+                    Style = "mole";
+                }
+                else {
+                    Style = "";
+                }
+            }
+        }
+    }
+}

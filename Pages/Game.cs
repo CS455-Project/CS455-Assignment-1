cd269677a849057d9149e2ff86dce96f17046070
arush <arush.up@gmail.com>
2024-08-11 13:03:19 +0530

    gitignore added

--- Pages/Game.cs
+++ Pages/Game.cs
@@ -4,7 +4,7 @@ namespace MoleProject.Pages
 {
     public partial class Game {
         public int score = 0;
-        public int currentTime = 60;
+        public int currentTime = 30;
         public int gameSpeed = 1000 / 2;
         public string msg = "";
         public int hitPosition = 0;
@@ -13,7 +13,7 @@ namespace MoleProject.Pages
         public List<SquareModel> Squares {get;set;} =  new List<SquareModel>();
 
         public Game(){
-            for ( int i = 0 ; i < 9 ; i++ ){
+            for ( int i = 0 ; i < 16 ; i++ ){
                 Squares.Add(new SquareModel{Id = i});
                 // System.Console.WriteLine(i);
             }

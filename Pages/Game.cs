71e779db865ab088d723f19fae2fe0a0a1107fcd
Akanksha Wattamwar <141128908+akankshabw@users.noreply.github.com>
2024-08-19 14:42:11 +0530

    Update Game.cs

--- Pages/Game.cs
+++ Pages/Game.cs
@@ -5,7 +5,7 @@ namespace MoleProject.Pages
     public partial class Game {
         public int score = 0;
         public int currentTime = 60;
-        public int gameSpeed = 500;
+        public int gameSpeed = 1000;
         public string msg = "";
         public int hitPosition = 0;
         public bool isGameRunning = true;

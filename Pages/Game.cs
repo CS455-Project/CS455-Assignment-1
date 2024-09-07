8bfeea6b94e1617719b26cdc13f9d5db41340580
Akanksha Wattamwar <akankshawattamwar4@gmail.com>
2024-09-07 11:49:19 +0530

    Refactored the code for Score as suggested by Sonarlint

--- Pages/Game.cs
+++ Pages/Game.cs
@@ -3,19 +3,26 @@ using MoleProject.Shared;
 namespace MoleProject.Pages
 {
     public partial class Game {
-        public int score = 0;
+
+        
         public int currentTime = 60;
         public int gameSpeed = 500;
         public string msg = "";
         public int hitPosition = 0;
         public bool isGameRunning = true;
 
+        private int score = 0;
+        public int Score{
+            get { return score; }
+            set { score = value; }
+        }
+
+
         public List<CellModel> Cells {get;set;} =  new List<CellModel>();
 
         public Game(){
             for ( int i = 0 ; i < 16 ; i++ ){
                 Cells.Add(new CellModel{Id = i});
-                // System.Console.WriteLine(i);
             }
         }
     }
--- obj/Debug/net8.0/MoleProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/MoleProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoleProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+753d7af1dad34251ef476d0109ce135ee6a3162e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cf12a86cb0a03a844946d1e40b5392583e42f50")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoleProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoleProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

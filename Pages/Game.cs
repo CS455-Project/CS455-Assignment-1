941e7b970a71ca1f0851a0f8cf6377f96750cf83
Akanksha Wattamwar <akankshawattamwar4@gmail.com>
2024-09-07 12:03:31 +0530

    Made the public fields private and used getters and setters

--- Pages/Game.cs
+++ Pages/Game.cs
@@ -4,19 +4,41 @@ namespace MoleProject.Pages
 {
     public partial class Game {
 
-        
-        public int currentTime = 60;
-        public int gameSpeed = 1000;
-        public string msg = "";
-        public int hitPosition = 0;
-        public bool isGameRunning = true;
+        private int currentTime = 60;
+        public int CurrentTime{
+            get { return currentTime; }
+            set { currentTime = value; }
+        }
+
+        private int gameSpeed = 1000;
+        public int GameSpeed{
+            get { return gameSpeed; }
+            set { gameSpeed = value; }
+        }
+
+        private string message = "";
+        public string Message{
+            get { return message; }
+            set { message = value; }
+        }
+
+        private int hitPosition = 0;
+        public int HitPosition{
+            get { return hitPosition; }
+            set { hitPosition = value; }
+        }
+
+        private bool isGameRunning = true;
+        public bool IsGameRunning{
+            get { return isGameRunning; }
+            set { isGameRunning = value; }
+        }
 
         private int score = 0;
         public int Score{
             get { return score; }
             set { score = value; }
         }
-
 
         public List<CellModel> Cells {get;set;} =  new List<CellModel>();
 

e16a4264672ee41fbf7d10f27c7464861bb8190b
A-Rush-R <arush.up@gmail.com>
2024-09-10 11:32:07 +0530

    game start test

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -3,15 +3,24 @@ using Xunit;
 
 public class WhackEmAllTests : TestContext
 {
+
     [Fact]
     public void StartGame_ShouldInitializeGameCorrectly()
     {
         var gameService = new Game();
         gameService.StartGame();
+        // Assert.Equal(60, gameService.currentTime);
 
         Assert.True(gameService.isGameRunning);
         Assert.Equal(0, gameService.score);
-        Assert.True(gameService.currentTime < 0);
+        Assert.Equal(750, gameService.gameSpeed);
+        Assert.Equal(16, gameService.Cells.Count);
+        Assert.True(gameService.isGameStarted);
+        Assert.True(string.IsNullOrEmpty(gameService.message));
+        Assert.False(gameService.showGameOverModal);
+        Assert.NotEqual(0, gameService.hitPosition);
+        Assert.NotNull(gameService.gameLoopTimer);
+        Assert.NotNull(gameService.gameTimeTimer); 
     }
 
     [Fact]

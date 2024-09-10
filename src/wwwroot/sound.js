6b43ceb7ee9faa54a47de2d04b077c733fde5a39
A-Rush-R <arush.up@gmail.com>
2024-09-10 18:47:51 +0530

    var to const

--- src/wwwroot/sound.js
+++ src/wwwroot/sound.js
@@ -1,11 +1,11 @@
 window.playSound1 = function () {
-    var sound = document.getElementById("hitSound");
+    const sound = document.getElementById("hitSound");
     sound.currentTime = 0; // Reset to start
     sound.play();
 };
 
 window.playSound2 = function () {
-    var sound = document.getElementById("missSound");
+    const sound = document.getElementById("missSound");
     sound.currentTime = 0; // Reset to start
     sound.play();
 };

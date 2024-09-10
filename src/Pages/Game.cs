500b04f6275e1115601e2fa9940b47a143dbcc17
A-Rush-R <arush.up@gmail.com>
2024-09-10 11:48:17 +0530

    made public for testing

--- src/Pages/Game.cs
+++ src/Pages/Game.cs
@@ -8,8 +8,12 @@ namespace MoleProject.Pages
         public int gameSpeed { get; set; } = 750;
         public string message { get; set; }
         public int hitPosition { get; set; } = 0;
-        public bool isGameRunning { get; set; }
+        public bool isGameRunning { get; set;} = false;
+        public bool isGameStarted {get;set;} = false;
+        public bool showGameOverModal {get;set;} = false;
         public int score { get; set; } = 0;
+        public PeriodicTimer? gameLoopTimer;
+        public PeriodicTimer? gameTimeTimer;
         public List<CellModel> Cells { get; set; } = new List<CellModel>();
 
         public Game()

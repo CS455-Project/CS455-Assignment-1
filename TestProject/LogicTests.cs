b8f08e86acdaeb2e16dd7277fd4eae0c5e11123a
Akanksha Wattamwar <akankshawattamwar4@gmail.com>
2024-09-12 10:01:40 +0530

    Renamed Test files

---
+++ TestProject/LogicTests.cs
@@ -0,0 +1,57 @@
+using Bunit;
+using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+using Moq;
+using System.Threading.Tasks;
+using MoleProject.Pages;
+using MoleProject.Shared;
+
+namespace GameTests{
+    public class GameTests: TestContext
+    {
+        private Mock<IJSRuntime> mockJSRuntime;
+
+        public GameTests()
+        {
+            mockJSRuntime = new Mock<IJSRuntime>();
+            Services.AddSingleton(mockJSRuntime.Object);
+        }
+
+        [Fact]
+        public void Constructor_InitializesCells()
+        {
+            var game = new Game();
+
+            Assert.Equal(16, game.Cells.Count);
+            Assert.All(game.Cells, cell => Assert.IsType<CellModel>(cell));
+        }
+
+        [Fact]
+        public async Task MouseUp_HitPosition_IncreaseScoreAndInvokeJavaScript()
+        {
+            var cut = RenderComponent<Game>();
+            cut.Instance.StartGame();
+            var hitCell = new CellModel { Id = cut.Instance.hitPosition };
+
+            await cut.Instance.MouseUp(hitCell);
+
+            Assert.Equal(1, cut.Instance.score);
+            mockJSRuntime.Verify(js => js.InvokeAsync<object>(It.IsAny<string>(), It.IsAny<object[]>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task MouseUp_MissPosition_InvokeJavaScript()
+        {
+            var cut = RenderComponent<Game>();
+            cut.Instance.StartGame();
+            var missCell = new CellModel { Id = (cut.Instance.hitPosition + 1) % 16 };
+
+            await cut.Instance.MouseUp(missCell);
+
+            Assert.Equal(0, cut.Instance.score);
+            mockJSRuntime.Verify(js => js.InvokeAsync<object>(It.IsAny<string>(), It.IsAny<object[]>()), Times.Once);
+        }
+
+    }
+}
---
+++ TestProject/SoundTests.cs
@@ -0,0 +1,83 @@
+using NUnit.Framework;
+using Moq;
+using System;
+
+namespace Audio
+{
+    public interface IAudioPlayer
+    {
+        void Play(string soundId);
+    }
+}
+
+namespace GameLogic
+{
+    public class SoundPlayer
+    {
+        private readonly Audio.IAudioPlayer _audioPlayer;
+
+        public SoundPlayer(Audio.IAudioPlayer audioPlayer)
+        {
+            _audioPlayer = audioPlayer;
+        }
+
+        public void PlaySound1()
+        {
+            _audioPlayer.Play("hitSound");
+        }
+
+        public void PlaySound2()
+        {
+            _audioPlayer.Play("missSound");
+        }
+    }
+}
+
+namespace SoundPlayerTests
+{
+    [TestFixture]
+    public class SoundPlayerTests
+    {
+        private Mock<Audio.IAudioPlayer>? _mockAudioPlayer;
+        private GameLogic.SoundPlayer? _soundPlayer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockAudioPlayer = new Mock<Audio.IAudioPlayer>();
+            _soundPlayer = new GameLogic.SoundPlayer(_mockAudioPlayer.Object);
+        }
+
+        [Test]
+        public void PlaySound1_ShouldPlayHitSound()
+        {
+            _soundPlayer!.PlaySound1();
+
+            _mockAudioPlayer!.Verify(x => x.Play("hitSound"), Times.Once);
+        }
+
+        [Test]
+        public void PlaySound2_ShouldPlayMissSound()
+        {
+            _soundPlayer!.PlaySound2();
+
+            _mockAudioPlayer!.Verify(x => x.Play("missSound"), Times.Once);
+        }
+
+        [Test]
+        public void PlaySound1_ShouldNotPlayMissSound()
+        {
+            _soundPlayer!.PlaySound1();
+
+            _mockAudioPlayer!.Verify(x => x.Play("missSound"), Times.Never);
+        }
+
+        [Test]
+        public void PlaySound2_ShouldNotPlayHitSound()
+        {
+            _soundPlayer!.PlaySound2();
+
+            _mockAudioPlayer!.Verify(x => x.Play("hitSound"), Times.Never);
+        }
+    }
+}

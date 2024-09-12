e1d926ca579553f5eb79f191d476147425ed206a
A-Rush-R <127140600+A-Rush-R@users.noreply.github.com>
2024-09-12 16:09:53 +0000

    Deploying to gh-pages from  @ 63eb3710cecc9d1d23196faf64db0bc9895ae0af 🚀

--- sound.js
+++ sound.js
@@ -1,11 +1,11 @@
 window.playSound1 = function () {
-    const sound = document.getElementById("hitSound");
+    var sound = document.getElementById("hitSound");
     sound.currentTime = 0; // Reset to start
     sound.play();
 };
 
 window.playSound2 = function () {
-    const sound = document.getElementById("missSound");
+    var sound = document.getElementById("missSound");
     sound.currentTime = 0; // Reset to start
     sound.play();
 };

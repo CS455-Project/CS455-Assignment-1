4bf962c5662ec41c7e4c5dd5479f8712d3239a25
Akanksha Wattamwar <akankshawattamwar4@gmail.com>
2024-10-09 13:07:05 +0530

    formatted the code

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+321700b0ac8fd4633da7523ade202a7b9ca655e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bccef9223ac9efad9af60d14dbf694779f3be12")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
